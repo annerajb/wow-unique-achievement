7e11d25034e76a076a7413a0e6bae206ce3bf27c
Anner J. Bonilla <annerjb@gmail.com>
2012-04-12 21:04:04 -0400

    Now achievements are displayed with subcategories instead of a giant list. apparently i broke the code to check for duplicate titles thought thinking of just having a array of ids for hode and alliance achievements

--- achi.py
+++ achi.py
@@ -3,7 +3,32 @@ import json
 import urllib2
 import itertools
 from pprint import pprint
-
+import md5, os, tempfile, time
+class DiskCacheFetcher:
+    def __init__(self, cache_dir=None):
+        # If no cache directory specified, use system temp directory
+        if cache_dir is None:
+            cache_dir = tempfile.gettempdir()
+        self.cache_dir = cache_dir
+    def fetch(self, url, max_age=0):
+        filename = md5.new(url).hexdigest()
+        filepath = os.path.join(self.cache_dir, filename)
+        if os.path.exists(filepath):
+            if int(time.time()) - os.path.getmtime(filepath) < max_age:
+                return open(filepath).read()
+        print url
+        # Retrieve over HTTP and cache, using rename to avoid collisions
+        data = urllib2.urlopen(url).read()
+        
+	print data
+	fd, temppath = tempfile.mkstemp()
+        fp = os.fdopen(fd, 'w')
+        fp.write(data)
+        fp.close()
+        os.rename(temppath, filepath)
+        return data
+        
+        
 base_url = 'http://us.battle.net/api/wow/character/'
 characters = [
                 ['kirin%20tor','annerajb'],
@@ -14,26 +39,29 @@ characters = [
                 ['sen\'jin','annerajbbank']
             ]
 earned = []
-achievements = {}
+
 titles = []
 points = 0
-def iterate_tree(tree,earned):
+def iterate_tree(subcategory,tree,earned):
+    temp_list = []
     for achi_category in tree:
-            #add the achievement to a dictionary with the key as the id
-            #
             if achi_category['id'] not in earned:
-                #dont add the achievement if we have a achievement title with it's name
+                #duplicate title
                 if achi_category['title'] not in titles:
-                    titles.append(achi_category['title']);
-                    achievements[achi_category['id']] = achi_category;
+                    titles.append(achi_category['title'])
+                    #build achievement list
+                    temp_list.append(achi_category)
                     global points
                     points += achi_category['points']
-                    #pprint(achi_category)
+    
+    subcategory['achievements'] = temp_list
+    #subcategory.append(temp_list)
 if __name__=="__main__":
     #merge all characters data
+    fetcher = DiskCacheFetcher('/')
     for char_file in characters:
         char_url = base_url + char_file[0]+'/'+char_file[1]+'?fields=achievements'
-        #print '||'+char_url + '||\n'
+        #data = fetcher.fetch(char_url,60);
         data = urllib2.urlopen(char_url)
         jdata = json.load(data)
         data.close()
@@ -46,15 +74,25 @@ if __name__=="__main__":
     achi_filename = open("achievements.json")
     achi_tree = json.load(achi_filename)
     achi_filename.close()
-    for categories in achi_tree['achievements']:
-        #skip feast of strenght i know this can be done with a slice but it's getting late
-        if categories['id'] == 81:
+    achievement_tree = {}
+    for main_categories in achi_tree['achievements']:
+        #skip feast of strenght
+        if main_categories['id'] == 81:
             continue
-        if 'categories' in categories:
-            for achi_under_category in categories['categories']:
-                iterate_tree(achi_under_category['achievements'],earned)
-        iterate_tree(categories['achievements'],earned)
-    pprint(achievements)
-    print len(achievements)
+        main_category = {'name':main_categories['name'], 'id':main_categories['id']}
+        #do we have a subcategory like quests -> eastern kingdom
+        if 'categories' in main_categories:
+            #loop thru all sub and add to main
+            for achi_under_category in main_categories['categories']:
+                subcategory = {'name': achi_under_category['name'], 'id': achi_under_category['id']}
+                iterate_tree(subcategory,achi_under_category['achievements'],earned)
+                #pprint(subcategory)
+                main_category['achievements'] = subcategory
+                
+        iterate_tree(main_category,main_categories['achievements'],earned)
+        pprint(main_category)
+        #achievement_tree.append(main_category)
+    #pprint(achievements)
+    #print len(achievements)
     print points
 
